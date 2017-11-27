0eac3f4a9db48df1dc7bb3073ffbc228b1e6251d
Julian Salinas <julian4001@outlook.com>
2017-11-27 13:19:29 -0600

    Cambio en la forma de guardar el archivo descriptivo

--- Proyecto II/model/dao_shotgun.py
+++ Proyecto II/model/dao_shotgun.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------------------------------------------------
-
+import json
 from util.file import *
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -12,41 +12,30 @@ class DAOShotgun(object):
     def __init__(self):
         self.archivo_entrada = "../files/entrada.txt"
         self.archivo_salida_fragmentos = "../files/salida.txt"
-        self.archivo_salida_descripcion = "../files/salida.pyd"
+        self.archivo_salida_descripcion = "../files/salida.json"
 
     # ------------------------------------------------------------------------------------------------------------------
 
-    def abrir_archivo_fragmentos(self, nombre_archivo=None):
-
-        if nombre_archivo is None:
-            nombre_archivo = self.archivo_entrada
-
+    def abrir_archivo_fragmentos(self, nombre_archivo):
         cadena = open_file(nombre_archivo)
-        cadena = cadena.replace("\n", " ")
-
-        return cadena
+        return cadena.replace("\n", " ")
 
     # ------------------------------------------------------------------------------------------------------------------
 
-    def guardar_archivo_fragmentos(self, fragmentos, nombre_archivo=None):
-
-        if nombre_archivo is None:
-            nombre_archivo = self.archivo_salida_fragmentos
-
+    def guardar_archivo_fragmentos(self, fragmentos, nombre_archivo):
         cadena = "".join([fragmento + "\n" for fragmento in fragmentos])
         save_file(nombre_archivo, cadena)
 
     # ------------------------------------------------------------------------------------------------------------------
 
-    def abrir_archivo_descriptivo(self, nombre_archivo=None):
-        diccionario = eval(open_file(nombre_archivo))
-        return diccionario
+    def abrir_archivo_descriptivo(self, nombre_archivo):
+        with open(nombre_archivo, 'r') as fp:
+            return json.load(fp)
 
     # ------------------------------------------------------------------------------------------------------------------
 
-    def guardar_archivo_descriptivo(self, diccionario=None, nombre_archivo=None):
-        contenido = str(diccionario)
-        save_file(nombre_archivo, contenido)
-        return diccionario
+    def guardar_archivo_descriptivo(self, diccionario, nombre_archivo):
+        with open(nombre_archivo, 'w') as fp:
+            json.dump(diccionario, fp, indent=4)
 
 # ----------------------------------------------------------------------------------------------------------------------
